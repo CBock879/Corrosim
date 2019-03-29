df3dc49a1b2ec0504339e260c259423e3471f906
christopher <christophercbock@gmail.com>
2019-03-29 11:55:01 -0400

    proper array dims

--- main.f95
+++ main.f95
@@ -1,12 +1,16 @@
 PROGRAM main
+        IMPLICIT NONE
+
+
+CONTAINS
 SUBROUTINE  RK4(x,t,dt,l)
       !x = x(t+dt)
 
       IMPLICIT NONE
       !size of the state matrix
-      INTEGER              :: l 
-      REAL                 :: dt, t
-      REAL, DIMENSION (l)  :: x,k1,k2,k3,k4
+      INTEGER                           :: l 
+      REAL                              :: dt, t
+      REAL, ALLOCATABLE, DIMENSION (l,3)  :: x,k1,k2,k3,k4
 
       k1 = dt * funct(x,t)
       k2 = dt * funct(x+k1/2,t+dt/2,l)
@@ -28,18 +32,18 @@ END SUBROUTINE
 !********************************************
 FUNCTION funct(state,t,l)
         IMPLICIT NONE
-        REAL, DIMENSION (l*3) :: funct, state  
+        REAL, ALLOCATABLE ,DIMENSION (l,3) :: funct, state  
         REAL                  :: t
         INTEGER               :: l,i
 
 
         !first element (Assumes closed region)
-        funct(1) = state(1) + elementval(state(l),state(1),state(2))        
+        funct(1,:) = state(1,:) + elementval(state(l,:),state(1,:),state(2,:))        
 
         DO i = 2, l-1, 1
-                funct(i) = state(i) + elementval(state(i-1),state(i),state(i+1))        
+                funct(i,:) = state(i,:) + elementval(state(i-1,:),state(i,:),state(i+1,:))        
         END DO
-        funct(l) = state(l) + elementval(state(l-1),state(l),state(1))
+        funct(l,:) = state(l) + elementval(state(l-1,:),state(l,:),state(1,:))
 
 
 END FUNCTION
@@ -55,7 +59,7 @@ END FUNCTION
 !Returns:
 !       value of the partial at that element
 !********************************************
-FUNCTION elementval(ua,ub,uc)
+FUNCTION elementval(ua,ub,uc,orientation)
         IMPLICIT NONE 
         REAL, DIMENSION(3)        :: ua,ub,uc,
         REAL, DIMENSION(3)        :: orientation
